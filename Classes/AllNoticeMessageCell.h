4d82aed7c7b00e3d45d0edf0588c62cec6c7930a
QBOBO <1474286853@qq.com>
2020-05-27 09:48:42 +0800

    类库

---
+++ Classes/AllNoticeMessageCell.h
@@ -0,0 +1,17 @@
+//
+//  AllNoticeMessageCell.h
+//  SafeEduIntroduce
+//
+//  Created by 王猛 on 2018/1/24.
+//  Copyright © 2018年 suifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllNoticeMessageCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *myTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *myContentLaebl;
+@property (weak, nonatomic) IBOutlet UILabel *myTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *redlable;
+
+@end
---
+++ Classes/AllNoticeMessageCell.m
@@ -0,0 +1,24 @@
+//
+//  AllNoticeMessageCell.m
+//  SafeEduIntroduce
+//
+//  Created by 王猛 on 2018/1/24.
+//  Copyright © 2018年 suifeng. All rights reserved.
+//
+
+#import "AllNoticeMessageCell.h"
+
+@implementation AllNoticeMessageCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
